70059a01942b7c2420d9ca5ed46bbdff27308448
Pedro vinicius V7 <pedrn770@gmail.com>
2024-06-29 15:34:20 -0300

    Sábado, 29 de junho de 2024

--- Dados.js
+++ Dados.js
@@ -2,7 +2,7 @@ function logar(){
     var login = document.getElementById('login').value;
     var senha = document.getElementById('senha').value;
 
-    if(login == 'Pedro' && senha == 'Vinicius'){
+    if(login == 'Kaua' && senha == 'lindao'){
         alert('Vamos entrar!!')
         location.href='/SIstema-escolar/Inter.html';
     }else{
